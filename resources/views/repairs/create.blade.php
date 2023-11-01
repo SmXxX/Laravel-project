5ebedb74de3698c1fa4a512512fcc3286a05b09c
Shener Hadjimehmed <shener.hadjimehmed@abv.bg>
2023-11-01 21:58:56 +0200

    TEST CHANGE

--- resources/views/repairs/create.blade.php
+++ resources/views/repairs/create.blade.php
@@ -14,97 +14,52 @@
                 <select class="mb-4 bg-transparent text-black outline-none text-center" name="car_id" id="car">
                     <option value="">Избери колa</option>
                     @foreach ($cars as $car)
-                    <option class="bg-transparent text-black outline-none text-center"  value="{{$car->id}}">{{$car->brand}}</option>
+                    <option class="bg-transparent text-black outline-none text-center" value="{{$car->id}}">{{$car->brand}}</option>
                     @endforeach
                 </select>
                 @error('car_id')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <label
-                    for="repair"
-                    class="inline-block text-lg mb-2"
-                    >Извършен ремонт</label
-                >
-                <input
-                    type="text"
-                    id="reprair"
-                    class="border border-gray-200 rounded p-2 w-full"
-                    name="repair"
-                    placeholder="Извършен ремонт"
-                />
+                <label for="repair" class="inline-block text-lg mb-2">Извършен ремонт</label>
+                <input type="text" id="reprair" class="border border-gray-200 rounded p-2 w-full" name="repair" placeholder="Извършен ремонт" />
                 @error('repair')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <label
-                    for="part"
-                    class="inline-block text-lg mb-2"
-                    >Сменена част</label
-                >
-                <input
-                    type="text"
-                    id="part"
-                    class="border border-gray-200 rounded p-2 w-full"
-                    name="part"
-                    placeholder="Сменена част"
-                />
+                <label for="part" class="inline-block text-lg mb-2">Сменена част</label>
+                <input type="text" id="part" class="border border-gray-200 rounded p-2 w-full" name="part" placeholder="Сменена част" />
                 @error('part')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <label for="kilometers" class="inline-block text-lg mb-2"
-                    >Километри</label
-                >
-                <input
-                    type="text"
-                    id="kilometers"
-                    class="border border-gray-200 rounded p-2 w-full"
-                    name="kilometers"
-                    placeholder="Километри"
-                />
+                <label for="kilometers" class="inline-block text-lg mb-2">Километри</label>
+                <input type="text" id="kilometers" class="border border-gray-200 rounded p-2 w-full" name="kilometers" placeholder="Километри" />
                 @error('kilometers')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <label for="work_cost" class="inline-block text-lg mb-2"
-                    >Цена труд</label
-                >
-                <input
-                    type="text"
-                    id="work_cost"
-                    class="border border-gray-200 rounded p-2 w-full"
-                    name="work_cost"
-                    placeholder="Цена труд"
-                />
+                <label for="work_cost" class="inline-block text-lg mb-2">Цена труд</label>
+                <div>test</div>
+                <input type="text" id="work_cost" class="border border-gray-200 rounded p-2 w-full" name="work_cost" placeholder="Цена труд" />
                 @error('work_cost')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
             </div>
             <div class="mb-6">
-                <label for="part_cost" class="inline-block text-lg mb-2"
-                    >Цена части</label
-                >
-                <input
-                    type="text"
-                    id="part_cost"
-                    class="border border-gray-200 rounded p-2 w-full"
-                    name="part_cost"
-                    placeholder="Цена части"
-                />
+                <label for="part_cost" class="inline-block text-lg mb-2">Цена части</label>
+                <input type="text" id="part_cost" class="border border-gray-200 rounded p-2 w-full" name="part_cost" placeholder="Цена части" />
                 @error('part_cost')
-                    <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+                <p class="text-red-500 text-xs mt-1">{{$message}}</p>
                 @enderror
             </div>
 
             <div class="mb-6">
-                <button
-                    class="bg-[#007CCA] text-white rounded py-2 px-4"
-                >
+                <button class="bg-[#007CCA] text-white rounded py-2 px-4">
                     Създай
                 </button>
 
